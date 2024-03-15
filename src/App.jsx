20164129e186acad4851639a462fa702786fe5c2
thkiaAdiba <athkiaadiba@gmail.com>
2024-03-16 00:42:23 +0600

    added banner section

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,32 @@ function App() {
         <Nevber></Nevber>
         <Banner></Banner>
       </div>
-      
+      <div className="card w-96 bg-base-100 border-2 border-[#28282833] mb-10 ml-10">
+        <figure className="px-5 pt-5">
+          <img src="https://res.cloudinary.com/dv6fgvj2c/image/upload/v1710524684/v3hli22jmyeafsmaa3la.jpg" alt="Shoes" className="rounded-xl w-full" />
+        </figure>
+        <div className="card-body">
+          <h2 className="card-title text-xl font-semibold text-[#282828]">Fattoush Salad</h2>
+          <p className="text-base text-[#878787]">2,000 calories a day is used for general nutrition advice, but calorie needs vary.</p>
+          <div className="divider"></div>
+          <h5 className="text-lg font-medium text-[#282828]">Ingredients: 6</h5>
+          <ul>
+            <li className='text-lg text-[#878787]'>Parmesan Cheese</li>
+            <li className='text-lg text-[#878787]'>Lettuce</li>
+            <li className='text-lg text-[#878787]'>Purslane</li>
+          </ul>
+          <div className='divider'></div>
+          <div className='flex justify-between items-center'>
+            <p className='text-[#282828cc]'>30 minutes</p>
+            <p className='text-[#282828cc]'>600 calories</p>
+          </div>
+
+          <div className='mt-4'>
+            <button className="btn bg-[#0BE58A] rounded-[50px] border-none mr-5">Want to Cook</button>
+          </div>
+        </div>
+      </div>
+
     </>
   )
 }
--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -1,14 +1,17 @@
-import banner from '../../assets/image/banner.jpg'
+
 const Banner = () => {
     return (
         <div className='mt-[50px] mb-[100px]'>
             <div className="hero h-[550px] rounded-3xl" style={{ backgroundImage: 'url(https://res.cloudinary.com/dv6fgvj2c/image/upload/v1710520169/dgdivbd1v56sx5v6nx00.jpg)' }}>
                 <div className="hero-overlay bg-opacity-15"></div>
                 <div className="hero-content text-center text-neutral-content">
-                    <div className="max-w-md">
-                        <h1 className="mb-5 text-5xl font-bold">Hello there</h1>
-                        <p className="mb-5">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
-                        <button className="btn btn-primary">Get Started</button>
+                    <div className="w-full">
+                        <h1 className="mb-5 text-4xl font-bold text-white">Discover an exceptional cooking <br /> class tailored for you!</h1>
+                        <p className="mb-5 text-center text-lg text-white">Learn and Master Basic Programming, Data Structures, Algorithm, OOP, Database and solve 500+ coding <br /> problems to become an exceptionally well world-class Programmer.</p>
+                        <div>
+                            <button className="btn bg-[#0BE58A] rounded-[50px] border-none mr-5">Explore Now</button>
+                            <button className="btn rounded-[50px] btn-outline border-white text-white">Our Feedback</button>
+                        </div>
                     </div>
                 </div>
             </div>
